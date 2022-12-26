cea205cd300b9505fed9f9dd06d35237f6362399
Сергей <123gendlyar35@gmail.com>
2022-12-26 14:28:30 +0400

    Initial commit

---
+++ crm_app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./crm_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+#engine = create_engine("mysql://iwatercrm:2bWcpBMau2bW3@95.213.183.180/iwater_test")
+
+engine = create_engine("mysql+mysqldb://iwatercrm:2bWcpBMau2bW3@95.213.183.180/iwater_test")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
