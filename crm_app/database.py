f0d2995b17d0f5efa5deea92c02d1bbd502bd03e
Сергей <123gendlyar35@gmail.com>
2022-12-26 14:36:17 +0400

    updated password

--- crm_app/database.py
+++ crm_app/database.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./crm_app.db"
 
 #engine = create_engine("mysql://iwatercrm:2bWcpBMau2bW3@95.213.183.180/iwater_test")
 
-engine = create_engine("mysql+mysqldb://iwatercrm:2bWcpBMau2bW3@95.213.183.180/iwater_test")
+engine = create_engine("postgresql+psycopg2://iwatercrm:2bWcpBMau2bW33@95.213.183.180/iwater_test")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
