c03759af30d8d824058cb80d6e9cf73bae884a4e
Сергей <123gendlyar35@gmail.com>
2022-12-26 16:06:50 +0400

    сделан вывод

--- crm_app/schemas.py
+++ crm_app/schemas.py
@@ -1,35 +1,43 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
+#
+# class ItemBase(BaseModel):
+#     title: str
+#     description: str | None = None
+#
+#
+# class ItemCreate(ItemBase):
+#     pass
+#
+#
+# class Item(ItemBase):
+#     id: int
+#     owner_id: int
+#
+#     class Config:
+#         orm_mode = True
+#
+#
+# class UserBase(BaseModel):
+#     email: str
+#
+#
+# class UserCreate(UserBase):
+#     password: str
+#
+#
+# class User(UserBase):
+#     id: int
+#     is_active: bool
+#     items: list[Item] = []
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
 
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
+class Notification(BaseModel):
     id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: list[Item] = []
+    date_create: datetime
+    company_id: int
 
     class Config:
         orm_mode = True
