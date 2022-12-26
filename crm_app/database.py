a294475c1cc67b098ecbc56fc1b6c5209561f081
Сергей <123gendlyar35@gmail.com>
2022-12-26 15:07:34 +0400

    непонятно и страшно

--- crm_app/database.py
+++ crm_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./crm_app.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./crm_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 #engine = create_engine("mysql://iwatercrm:2bWcpBMau2bW3@95.213.183.180/iwater_test")
